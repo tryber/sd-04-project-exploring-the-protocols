6356324fe35e56a5f8b7a27d4dfd2e5f283dc5ce
leandromarten1 <leandromarten1@gmail.com>
2021-01-12 18:07:04 -0300

    fix lint

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ const net = require('net');
 const { getLocationInfos } = require('./location');
 
 const getHeaderValue = (data, header) => {
-  console.log('data = ', data);
+  console.log(data);
   const headerData = data
     .split('\r\n')
     .find((chunk) => chunk.startsWith(header));
@@ -11,8 +11,7 @@ const getHeaderValue = (data, header) => {
   return headerData.split(': ').pop();
 };
 
-const startOfResponse =
-  'HTTP/1.1 200 OK/r/nContent-Type: text/html; charset=UTF-8/r/n/r/n';
+const startOfResponse = 'HTTP/1.1 200 OK/r/nContent-Type: text/html; charset=UTF-8/r/n/r/n';
 
 const endOfResponse = '/r/n/r/n';
 
