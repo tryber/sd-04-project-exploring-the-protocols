845a6bcf2dca0347c07570edf4ecd1833f14dcfb
leandromarten1 <leandromarten1@gmail.com>
2021-01-12 18:31:23 -0300

    req 10

--- src/index.js
+++ src/index.js
@@ -20,13 +20,27 @@ const server = net.createServer((socket) => {
     const clientIP = getHeaderValue(data.toString(), 'X-Forwarded-For');
 
     getLocationInfos(clientIP, (locationData) => {
+      const {
+        ip,
+        city,
+        postal_code: zip,
+        region,
+        region_name: state,
+        country_name: country,
+        company,
+      } = locationData;
       socket.write(startOfResponse);
       socket.write(
         '<html><head><meta http-equiv="content-type" content="text/html;charset=utf-8">',
       );
       socket.write('<title>Trybe 🚀</title></head><body>');
       socket.write('<H1>Explorando os Protocolos 🧐🔎</H1>');
-      socket.write(`<p data-testid="ip">${locationData.ip}</p>`);
+      socket.write(`<p data-testid="ip">${ip}</p>`);
+      socket.write(`<p data-testid="city">${city}</p>`);
+      socket.write(`<p data-testid="postal_code">${zip}</p>`);
+      socket.write(`<p data-testid="region">${state}, ${region}</p>`);
+      socket.write(`<p data-testid="country"">${country}</p>`);
+      socket.write(`<p data-testid="company"">${company}</p>`);
       socket.write(
         '<iframe src="https://giphy.com/embed/l3q2zVr6cu95nF6O4" width="480" height="236" frameBorder="0" class="giphy-embed" allowFullScreen></iframe>',
       );
