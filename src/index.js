73f00f1105c05c7e1510f90d736a5c0bdb2d94e4
mirellaalves <mirella.alves@gmail.com>
2021-01-14 17:22:32 -0300

    requirement 8 to 11, and correct \s on start and endOfResponse

--- src/index.js
+++ src/index.js
@@ -10,22 +10,27 @@ const getHeaderValue = (data, header) => {
   return headerData.split(': ').pop();
 };
 
-const startOfResponse = 'HTTP/1.1 200 OK\\r\\nContent-Type: text/html; charset=UTF-8\\r\\n\\r\\n';
+const startOfResponse = 'HTTP/1.1 200 OK\r\nContent-Type: text/html; charset=UTF-8\r\n\r\n';
 
-const endOfResponse = '\\r\\n\\r\\n';
+const endOfResponse = '\r\n\r\n';
 
 const server = net.createServer((socket) => {
   socket.on('data', (data) => {
-    const clientIP = null;
+    const clientIP = getHeaderValue(data.toString(), 'X-Forwarded-For');
 
     getLocationInfos(clientIP, (locationData) => {
       socket.write(startOfResponse);
-      socket.write('<html><head><meta http-equiv="content-type"
-content="text/html;charset=utf-8">'); (junta com a de cima, com espaço)
+      socket.write('<html><head><meta http-equiv="content-type"content="text/html;charset=utf-8">');
       socket.write('<title>Trybe 🚀</title></head><body>');
       socket.write('<H1>Explorando os Protocolos 🧐🔎</H1>');
       socket.write('<iframe src="https://giphy.com/embed/l3q2zVr6cu95nF6O4" width="480" height="236" frameBorder="0" class="giphy-embed" allowFullScreen></iframe>');
       socket.write('</body></html>');
+      socket.write(`<h1 data-testid="ip">${clientIP}</h1>`);
+      socket.write(`<h1 data-testid="city">${locationData.city}</h1>`);
+      socket.write(`<h1 data-testid="postal_code">${locationData.postal_code}</h1>`);
+      socket.write(`<h1 data-testid="region">${locationData.region}</h1>`);
+      socket.write(`<h1 data-testid="country">${locationData.country}</h1>`);
+      socket.write(`<h1 data-testid="company">${locationData.company}</h1>`);
       socket.write(endOfResponse);
     });
   });
