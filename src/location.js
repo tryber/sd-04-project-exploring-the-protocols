92323c66d1500d39b8c7d856ce9a45a9d003a8c8
mirellaalves <mirella.alves@gmail.com>
2021-01-14 15:17:57 -0300

    requirement 5

--- src/location.js
+++ src/location.js
@@ -1,7 +1,11 @@
 const https = require('https');
 
 const options = {
-  //
+  hostname: 'iplocation.com',
+  port: 443,
+  path: '/',
+  method: 'POST',
+  'Content-Type': 'application/x-www-form-urlencoded',
 };
 
 const getLocationInfos = (clientIP, cb) => {
