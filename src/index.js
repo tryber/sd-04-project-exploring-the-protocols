5d32459e64d4c93cdbe5281b1238a522020aa01b
Filipe de Castro <filipeyoga@gmail.com>
2021-01-14 11:26:42 -0300

    req6-7

--- src/index.js
+++ src/index.js
@@ -10,9 +10,10 @@ const { getLocationInfos } = require('./location');
 //   return headerData.split(': ').pop();
 // };
 
-const startOfResponse = null;
+const startOfResponse =
+  'HTTP/1.1 200 OK/r/nContent-Type: text/html; charset=UTF-8/r/n/r/n';
 
-const endOfResponse = null;
+const endOfResponse = '/r/n/r/n';
 
 const server = net.createServer((socket) => {
   socket.on('data', () => {
