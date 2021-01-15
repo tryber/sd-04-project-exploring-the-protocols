72e07150c48d7361fe53edf9af8516c4a0ac40da
deboracosilveira <deboracosilveira@gmail.com>
2021-01-15 18:54:26 -0300

    Req 10 & 11

--- src/index.js
+++ src/index.js
@@ -18,12 +18,18 @@ const server = net.createServer((socket) => {
   socket.on('data', (data) => {
     const clientIP = getHeaderValue(data.toString(), 'X-Forwarded-For');
 
-    getLocationInfos(clientIP, (/* locationData */) => {
+    getLocationInfos(clientIP, (locationData) => {
       socket.write(startOfResponse);
       socket.write('<html><head><meta http-equiv="content-type" content="text/html;charset=utf-8">');
       socket.write('<title>Trybe 🚀</title></head><body>');
       socket.write('<H1>Explorando os Protocolos 🧐🔎</H1>');
       socket.write('<iframe src="https://giphy.com/embed/l3q2zVr6cu95nF6O4" width="480" height="236" frameBorder="0" class="giphy-embed" allowFullScreen></iframe>');
+      socket.write(`<p data-testid="ip">${locationData.ip}</p>`);
+      socket.write(`<p data-testid="city">${locationData.city}</p>`);
+      socket.write(`<p data-testid="postal_code">${locationData.postal_code}</p>`);
+      socket.write(`<p data-testid="region">${locationData.region}</p>`);
+      socket.write(`<p data-testid="country">${locationData.country}</p>`);
+      socket.write(`<p data-testid="company">${locationData.company}</p>`);
       socket.write('</body></html>');
       socket.write(endOfResponse);
     });
