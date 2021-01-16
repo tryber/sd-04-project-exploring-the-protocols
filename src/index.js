2db5ade7bb7d8d7c32b751b1412d0a4ba3a233df
mathieu bouhelier <mathieubouhelier@gmail.com>
2021-01-16 10:15:32 -0300

    my first commit

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ const net = require('net');
 const { getLocationInfos } = require('./location');
 
 const getHeaderValue = (data, header) => {
+  
   const headerData = data
     .split('\r\n')
     .find((chunk) => chunk.startsWith(header));
