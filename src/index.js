93f1b5089b180a48ad86cdc34f9ea4491f651964
gabrielukke <gabriellukk@outlook.com>
2021-01-16 18:36:45 -0300

    Requirement 6

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ const { getLocationInfos } = require('./location');
   return headerData.split(': ').pop();
 }; */
 
-const startOfResponse = null;
+const startOfResponse = 'HTTP/1.1 200 OK\r\nContent-Type: text/html; charset=UTF-8\r\n\r\n';
 
 const endOfResponse = null;
 
