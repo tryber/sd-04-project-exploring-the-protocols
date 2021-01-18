9328cb4e47c2038244fed9e62f58f629c6c528fd
Mateus-Moreira <ashuraw@hotmail.com>
2021-01-18 19:48:22 -0300

    req 6, 7, 8

--- src/index.js
+++ src/index.js
@@ -2,13 +2,13 @@ const net = require('net');
 
 const { getLocationInfos } = require('./location');
 
-const startOfResponse = null;
+const startOfResponse = 'HTTP/1.1 200 OK\r\nContent-Type: text/html; charset=UTF-8\r\n\r\n';
 
-const endOfResponse = null;
+const endOfResponse = '\r\n\r\n';
 
 const server = net.createServer((socket) => {
   socket.on('data', (_data) => {
-    const clientIP = null;
+    const clientIP = getHeaderValue(data.toString(), 'X-Forwarded-For');
 
     getLocationInfos(clientIP, (_locationData) => {
       socket.write(startOfResponse);
