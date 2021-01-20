055a0aafde5a668718f21809bdecb040e418392c
Victor L <victor.lustosacp@gmail.com>
2021-01-20 09:41:28 -0300

    lint

--- src/index.js
+++ src/index.js
@@ -9,13 +9,12 @@ const getHeaderValue = (data, header) => {
 
   return headerData.split(': ').pop();
 };
-const startOfResponse =
-  'HTTP/1.1 200 OK\r\nContent-Type: text/html; charset=UTF-8\r\n\r\n';
+const startOfResponse = 'HTTP/1.1 200 OK\r\nContent-Type: text/html; charset=UTF-8\r\n\r\n';
 
 const endOfResponse = '\r\n\r\n';
 
 const server = net.createServer((socket) => {
-  socket.on('data', (_data) => {
+  socket.on('data', (data) => {
     const clientIP = getHeaderValue(data.toString(), 'X-Forwarded-For');
 
     getLocationInfos(clientIP, (_locationData) => {
@@ -25,7 +24,7 @@ const server = net.createServer((socket) => {
       );
       socket.write('<title>Trybe 🚀</title></head><body>');
       socket.write('<H1>Explorando os Protocolos 🧐🔎</H1>');
-      socket.write(`<p data-testid="ip">${clientIP}<p>`)
+      socket.write(`<p data-testid="ip">${clientIP}<p>`);
       socket.write(
         '<iframe src="https://giphy.com/embed/l3q2zVr6cu95nF6O4" width="480" height="236" frameBorder="0" class="giphy-embed" allowFullScreen></iframe>'
       );
