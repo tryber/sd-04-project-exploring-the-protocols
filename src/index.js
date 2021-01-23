4438234205c0c291815fff00c8944258c1120edd
igorgbr <igorgphotoarte@gmail.com>
2021-01-23 13:37:01 -0300

    req 11

--- src/index.js
+++ src/index.js
@@ -10,7 +10,8 @@ const getHeaderValue = (data, header) => {
   return headerData.split(': ').pop();
 };
 
-const startOfResponse = 'HTTP/1.1 200 OK\r\nContent-Type: text/html; charset=UTF-8\r\n\r\n';
+const startOfResponse =
+  'HTTP/1.1 200 OK\r\nContent-Type: text/html; charset=UTF-8\r\n\r\n';
 
 const endOfResponse = '\r\n\r\n';
 
@@ -18,17 +19,23 @@ const server = net.createServer((socket) => {
   socket.on('data', (data) => {
     const clientIP = getHeaderValue(data.toString(), 'X-Forwarded-For');
 
-    getLocationInfos(clientIP, () => {
+    getLocationInfos(clientIP, (locationData) => {
+      const { city, region, company } = locationData;
       socket.write(startOfResponse);
       socket.write(
-        '<html><head><meta http-equiv="content-type" content="text/html;charset=utf-8">',
+        '<html><head><meta http-equiv="content-type" content="text/html;charset=utf-8">'
       );
       socket.write('<title>Trybe 🚀</title></head><body>');
       socket.write('<H1>Explorando os Protocolos 🧐🔎</H1>');
       socket.write(
-        '<iframe src="https://giphy.com/embed/l3q2zVr6cu95nF6O4" width="480" height="236" frameBorder="0" class="giphy-embed" allowFullScreen></iframe>',
+        '<iframe src="https://giphy.com/embed/l3q2zVr6cu95nF6O4" width="480" height="236" frameBorder="0" class="giphy-embed" allowFullScreen></iframe>'
       );
       socket.write(`<p data-testid="ip">${clientIP}</p>`);
+      socket.write(`<p data-testid="city">${city}</p>`);
+      socket.write(`<p data-testid="postal_code">${locationData.postal_code}</p>`);
+      socket.write(`<p data-testid="region">${region}</p>`);
+      socket.write(`<p data-testid="country">${locationData.country_name}</p>`);
+      socket.write(`<p data-testid="company">${company}</p>`);
       socket.write('</body></html>');
       socket.write(endOfResponse);
     });
