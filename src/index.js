6c9f17eee3700551b34f6feaf608ad49e5d477ee
Julio Cesar Tinin <juliocesartinin@pop-os.localdomain>
2021-02-26 00:17:56 -0300

    testando req 11

--- src/index.js
+++ src/index.js
@@ -19,7 +19,7 @@ const server = net.createServer((socket) => {
     const clientIP = getHeaderValue(data.toString(), 'X-Forwarded-For');
 
     getLocationInfos(clientIP, (locationData) => {
-      const { city, region, country_name, company } = locationData;
+      const { city, region, country_name:countryName, postal_code: postalCode, company } = locationData;
       socket.write(startOfResponse);
       socket.write(
         '<html><head><meta http-equiv="content-type" content="text/html;charset=utf-8">',
@@ -27,9 +27,10 @@ const server = net.createServer((socket) => {
       socket.write('<title>Trybe 🚀</title></head><body>');
       socket.write('<H1>Explorando os Protocolos 🧐🔎</H1>');
       socket.write(`<p data-testid="ip">${clientIP}</p>`);
+      socket.write(`<p data-testid="postal_code">${postalCode}</p>`)
       socket.write(`<p data-testid="city">${city}</p>`);
       socket.write(`<p data-testid="region">${region}</p>`);
-      socket.write(`<p data-testid="country">${country_name}</p>`);
+      socket.write(`<p data-testid="country">${countryName}</p>`);
       socket.write(`<p data-testid="company">${company}</p>`);
       socket.write(
         '<iframe src="https://giphy.com/embed/l3q2zVr6cu95nF6O4" width="480" height="236" frameBorder="0" class="giphy-embed" allowFullScreen></iframe>',
