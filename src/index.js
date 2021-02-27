82161fd0f8700cbf1611bd281d9434d1ff4ed40e
richardwellerson <richardwellerson@gmail.com>
2021-02-27 08:35:38 -0300

    Req 6 e 7

--- src/index.js
+++ src/index.js
@@ -10,9 +10,12 @@ const getHeaderValue = (data, header) => {
   return headerData.split(': ').pop();
 };
 
-const startOfResponse = null;
+const startOfResponse = `${[
+  'HTTP/1.1 200 OK',
+  'Content-Type: text/html; charset=UTF-8',
+].join('\r\n')}`;
 
-const endOfResponse = null;
+const endOfResponse = '\r\n\r\n';
 
 const server = net.createServer((socket) => {
   socket.on('data', (data) => {
