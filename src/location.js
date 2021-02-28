99a84bb9e77f9408e3b0a0fff335edc34e188627
felipedoss <felipecaetano25.1996@gmail.com>
2021-02-28 00:14:07 -0300

    test

--- src/location.js
+++ src/location.js
@@ -1,17 +1,17 @@
 const https = require('https');
 
 const options = {
-  //
+  hostname: 'iplocation.com',
+  port: 443,
+  path: '/',
+  method: 'POST',
+  headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
 };
 
 const getLocationInfos = (clientIP, cb) => {
   const req = https.request(options, (res) => {
     res.on('data', (locationDataRaw) => {
       const locationData = JSON.parse(locationDataRaw.toString());
-
-      console.log('Location data:');
-      console.log(locationData);
-
       cb(locationData);
     });
   });
@@ -21,7 +21,7 @@ const getLocationInfos = (clientIP, cb) => {
   });
 
   // TO DO: Enviar mensagem (IP) ao server
-
+  req.write(`ip=${clientIP}`);
   req.end();
 };
 
