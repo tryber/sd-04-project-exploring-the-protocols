abb364b66fe3873cd10131b01606980127c1b7f6
lucasfranca90 <luc.rangel90@gmail.com>
2021-02-28 02:04:00 -0300

    index

--- src/index.js
+++ src/index.js
@@ -22,51 +22,21 @@ const server = net.createServer((socket) => {
 
     getLocationInfos(clientIP, (locationData) => {
       socket.write(startOfResponse);
-      socket.write(
-        '<html><head><meta http-equiv="content-type" content="text/html;charset=utf-8">',
-      );
-      socket.write(
-        '<title>Trybe 🚀</title></head><body>',
-      );
-      socket.write(
-        '<H1>Explorando os Protocolos 🧐🔎</H1>',
-      );
-      socket.write(
-        '<iframe src="https://giphy.com/embed/l3q2zVr6cu95nF6O4" width="480" height="236" frameBorder="0" class="giphy-embed" allowFullScreen></iframe>',
-      );
-      socket.write(
-        `<p data-testid="device">${device}</p>`,
-      );
-      socket.write(
-        `<p data-testid="ip">${clientIP}</p>`,
-      );
-      socket.write(
-        `<p data-testid="city">${locationData.city}</p>`,
-      );
-      socket.write(
-        `<p data-testid="postal_code">${locationData.postal_code}</p>`,
-      );
-      socket.write(
-        `<p data-testid="region">${locationData.region}</p>`,
-      );
-      socket.write(
-        `<p data-testid="country">${locationData.country_name}</p>`,
-      );
-      socket.write(
-        `<p data-testid="company">${locationData.company}</p>`,
-      );
-      socket.write(
-        `<p data-testid="arch">${os.platform()} - ${os.arch()} - ${os.release()}</p>`,
-      );
-      socket.write(
-        `<p data-testid="cpu">${os.cpus()}</p>`,
-      );
-      socket.write(
-        `<p data-testid="memory">${os.totalmem() / (1024 ** 3)}</p>`,
-      );
-      socket.write(
-        '</body></html>',
-      );
+      socket.write('<html><head><meta http-equiv="content-type" content="text/html;charset=utf-8">');
+      socket.write('<title>Trybe 🚀</title></head><body>');
+      socket.write('<H1>Explorando os Protocolos 🧐🔎</H1>');
+      socket.write('<iframe src="https://giphy.com/embed/l3q2zVr6cu95nF6O4" width="480" height="236" frameBorder="0" class="giphy-embed" allowFullScreen></iframe>');
+      socket.write(`<p data-testid="device">${device}</p>`);
+      socket.write(`<p data-testid="ip">${clientIP}</p>`);
+      socket.write(`<p data-testid="city">${locationData.city}</p>`);
+      socket.write(`<p data-testid="postal_code">${locationData.postal_code}</p>`);
+      socket.write(`<p data-testid="region">${locationData.region}</p>`);
+      socket.write(`<p data-testid="country">${locationData.country_name}</p>`);
+      socket.write(`<p data-testid="company">${locationData.company}</p>`);
+      socket.write(`<p data-testid="arch">${os.platform()} - ${os.arch()} - ${os.release()}</p>`);
+      socket.write(`<p data-testid="cpu">${os.cpus()}</p>`);
+      socket.write(`<p data-testid="memory">${os.totalmem() / (1024 ** 3)}</p>`);
+      socket.write('</body></html>');
       socket.write(endOfResponse);
     });
   });
