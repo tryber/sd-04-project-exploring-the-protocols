2ae1f0e169ab6a52bcf92d0a18628e2e0befe454
Kaecio <kaecio@pop-os.localdomain>
2021-03-01 02:27:59 -0300

    Tentando rodar alguns req

--- src/index.js
+++ src/index.js
@@ -2,31 +2,38 @@ const net = require('net');
 
 const { getLocationInfos } = require('./location');
 
-/* const getHeaderValue = (data, header) => {
+const getHeaderValue = (data, header) => {
   const headerData = data
     .split('\r\n')
     .find((chunk) => chunk.startsWith(header));
 
   return headerData.split(': ').pop();
-}; */
+};
 
-const startOfResponse = `${[
-  'HTTP/1.1 200 OK',
-  'Content-Type: text/html; charset=UTF-8',
-].join('\r\n')}\r\n\r\n`;
+const startOfResponse = 'HTTP/1.1 200 OK\r\nContent-Type: text/html; charset=UTF-8\r\n\r\n';
 
 const endOfResponse = '\r\n\r\n';
 
 const server = net.createServer((socket) => {
-  socket.on('data', (_data) => {
-    const clientIP = null;
+  socket.on('data', (data) => {
+    const clientIP = getHeaderValue(data.toString(), 'X-Forwarded-For');
 
-    getLocationInfos(clientIP, (_locationData) => {
+    getLocationInfos(clientIP, (locationData) => {
+      const {
+        postal_code: postalCode, city, region, country_name: country, company } = locationData;
       socket.write(startOfResponse);
       socket.write('<html><head><meta http-equiv="content-type" content="text/html;charset=utf-8">');
       socket.write('<title>Trybe 🚀</title></head><body>');
       socket.write('<H1>Explorando os Protocolos 🧐🔎</H1>');
-      socket.write('<iframe src="https://giphy.com/embed/l3q2zVr6cu95nF6O4" width="480" height="236" frameBorder="0" class="giphy-embed" allowFullScreen></iframe>');
+      socket.write(`<h2 data-testid="ip">${clientIP}</h2>`);
+      socket.write(`<h3 data-testid="city">${city}</h3>`);
+      socket.write(`<p data-testid="postal_code">${postalCode}</p>`);
+      socket.write(`<p data-testid="region">${region}</p>`);
+      socket.write(`<p data-testid="country">${country}</p>`);
+      socket.write(`<p data-testid="company">${company}</p>`);
+      socket.write(
+        '<iframe src="https://giphy.com/embed/l3q2zVr6cu95nF6O4" width="480" height="236" frameBorder="0" class="giphy-embed" allowFullScreen></iframe>'
+      );
       socket.write('</body></html>');
       socket.write(endOfResponse);
     });
