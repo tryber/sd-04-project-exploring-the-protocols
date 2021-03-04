abc497624c3f68a6d1e709350f22d6af403deca5
Rangel Paolo <paolorangel1996@outlook.com>
2021-03-04 15:20:18 -0300

    Req 11 I think

--- src/index.js
+++ src/index.js
@@ -21,12 +21,24 @@ const server = net.createServer((socket) => {
     const clientIP = getHeaderValue(data.toString(), 'X-Forwarded-For');
     // const something = getLocationInfos(clientIP);
 
-    getLocationInfos(clientIP, (/* locationData */) => {
+    getLocationInfos(clientIP, (locationData) => {
+      const {
+        city,
+        potal_code: postalCode,
+        region,
+        country_name: countryName,
+        company,
+      } = locationData;
       socket.write(startOfResponse);
       socket.write('<html><head><meta http-equiv="content-type" content="text/html;charset=utf-8">');
       socket.write('<title>Trybe 🚀</title></head><body>');
       socket.write('<H1>Explorando os Protocolos 🧐🔎</H1>');
       socket.write(`<h2 data-testid="ip">${clientIP}</h2>`);
+      socket.write(`<p data-testid="city">${city}</p>`);
+      socket.write(`<p data-testid="postal_code">${postalCode}</p>`);
+      socket.write(`<p data-testid="region">${region}</p>`);
+      socket.write(`<p data-testid="country">${countryName}</p>`);
+      socket.write(`<p data-testid="company">${company}</p>`);
       socket.write('<iframe src="https://giphy.com/embed/l3q2zVr6cu95nF6O4" width="480" height="236" frameBorder="0" class="giphy-embed" allowFullScreen></iframe>');
       socket.write('</body></html>');
       socket.write(endOfResponse);
